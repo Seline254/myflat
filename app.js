50632871aaa8af5bf3db34695c5da496c4e22c95
Seline254 <selineowino36@gmail.com>
2025-07-11 11:59:19 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Entry file
+const express = require("express")
+const mongoose = require("mongoose")
+const cors= require("cors")
+require('dotenv').config()
+
+// middleware
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// static files accessibility
+app.use("/uploads",express.static)
+
+// register/login route
+const userAuth = require("./routes/loginRoute")
+app.use('/user/auth/',userAuth)
+
+
+// connection to the database
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("Mongo Connected"))
+.catch(err=>console.log("Mongo Connection error"))
+
+const PORT = 3000
+app.listen(PORT,()=>{
+    console.log("Server running on port "+PORT+"...")
+})
---
+++ model/apartmentDB.js
@@ -0,0 +1,62 @@
+// schemas-(users,apartment,reviews,deliton)
+// user schema
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+// define user schema
+const userSchema = new Schema({
+    name:{type:String, required:true},
+    email:{type:String,required:true, unique:true},
+    phone:String,
+    password:{type:String,required:true},
+    isActive:{type:Boolean,default:true},
+    role:{type:String,enum:['admin','landlord','tenant'],required:true},
+},{timestamps:true}) //handles created at and updated at automatically.
+
+// Apartment Schema
+const apartmentSchema = new Schema({
+    title:{type:String,required:true},
+    description:{type:String},
+    type:String, // like a one-bedroom e.t.c
+    rent:{type:Number},
+    location:{type:String},
+    images:{String},
+    isAvailable:{type:Boolean,default:true},
+    landlord:{type:mongoose.Schema.Types.ObjectId,ref:'User', required:true},
+    review:{type:mongoose.Schema.Types.ObjectId,ref:'Review'},
+},{timestamps:true})
+
+// Review Schema
+const reviewSchema = new Schema({
+    tenant:{type:mongoose.Schema.Types.ObjectId,ref:'User', required:true},
+    apartment:{type:mongoose.Schema.Types.ObjectId,ref:'Apartment', required:true},
+    rating:{type:Number,min:1,max:5,required:true},
+    comment:String,
+    isFlagged:{type:Boolean,default:false},
+    isResolved:{type:Boolean,default:false},
+    isDeleted:{type:Boolean,default:false},
+    DeletionFeePaid:{type:Boolean,default:false},
+},{timestamps:true})
+
+// Deletion Schema
+const deletionSchema = new Schema({
+    review:{type:mongoose.Schema.Types.ObjectId,ref:'Review'},
+    landlord:{type:mongoose.Schema.Types.ObjectId,ref:'User', required:true},
+    reason:String,
+    status:{type:String,enum:['pending','approved','rejected'],default:"pending"},
+    paymentStatus:{type:String,enum:['unpaid','paid'],default:"paid"}
+},{timestamps:true})
+
+
+// prepare exports
+const User = mongoose.model("User",userSchema)
+const Apartment = mongoose.model("Apartment",apartmentSchema)
+const Review = mongoose.model("Review",reviewSchema)
+const Deletion = mongoose.model("Deletion",deletionSchema)
+
+module.exports = {User,Apartment,Review,Deletion}
+
+
+
+
+
